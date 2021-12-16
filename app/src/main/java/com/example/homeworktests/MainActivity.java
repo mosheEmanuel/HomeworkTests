884a213b204cb62dcb901a4c3421d776b78e6e8f
User <navgnbutl221@gmail.com>
2021-12-16 21:14:11 +0200

    testeng

---
+++ app/src/main/java/com/example/homeworktests/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.homeworktests;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etfname, etLname;
+    TextView tvClass;
+    String[] classArray = {"ז", "ח", "ט", "י", "יא", "יב"};
+    String theChoice = null;
+    boolean isDan = true;
+    Button btnxet;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (!isDan) {
+            Intent intent = new Intent(this, MenuActivity.class);
+            startActivity(intent);
+        }
+        init();
+    }
+
+    public void init() {
+        etfname = findViewById(R.id.etfname);
+        etLname = findViewById(R.id.etLname);
+        tvClass = findViewById(R.id.tvClass);
+        btnxet = findViewById(R.id.btnxet);
+
+        tvClass.setOnClickListener(this);
+        btnxet.setOnClickListener(this);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        if (v == tvClass)
+            selectedClass();
+        else if (v == btnxet) {
+            if (!etLname.toString().isEmpty() && !etfname.toString().isEmpty()) {
+                isDan = false;
+                Intent intent = new Intent(this, MenuActivity.class);
+                startActivity(intent);
+            }
+        }
+    }
+
+
+    public void selectedClass() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("תבחר כיתה");
+        builder.setCancelable(false);
+
+        builder.setSingleChoiceItems(classArray, 0, (dialog, which) -> theChoice = classArray[which]);
+
+        builder.setPositiveButton("OK", (dialog, which) -> tvClass.setText(theChoice)
+        );
+
+        builder.setNegativeButton("cancel", null);
+        builder.show();
+    }
+
+
+}
