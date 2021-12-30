c570d660d452b960e8f50d8b3b9847eda21fa581
student <navgnbutk221@gmail.com>
2021-12-30 16:23:44 +0200

    שדרוגים קטנים

--- app/src/main/java/com/example/homeworktests/MainActivity.java
+++ app/src/main/java/com/example/homeworktests/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         sp = getSharedPreferences("isDan", 0);
