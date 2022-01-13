6ea5d7bbac513586e68ab6fd632274649e863d70
student <navgnbutk221@gmail.com>
2022-01-13 17:54:49 +0200

    end HomeworkAdapter

--- app/src/main/java/com/example/homeworktests/MenuActivity.java
+++ app/src/main/java/com/example/homeworktests/MenuActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MenuActivity extends AppCompatActivity {
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        fab = fab.findViewById(R.id.fab);
+
     }
 }
