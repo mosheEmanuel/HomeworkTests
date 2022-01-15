dd3f5df546be8dad5599fc67ce0b7fd1103c7621
User <navgnbutl221@gmail.com>
2022-01-15 23:28:05 +0200

    na

--- app/src/main/java/com/example/homeworktests/MenuActivity.java
+++ app/src/main/java/com/example/homeworktests/MenuActivity.java
@@ -1,19 +1,48 @@
 package com.example.homeworktests;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity{
     FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        fab = fab.findViewById(R.id.fab);
+        fab = findViewById(R.id.fab);
+       registerForContextMenu(fab);
+    }
 
+
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.popup_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.add_homework)
+        {
+            return true;
+        }
+       else if(item.getItemId()==R.id.add_homework)
+        {
+            return true;
+        }
+
+            return false;
     }
 }
