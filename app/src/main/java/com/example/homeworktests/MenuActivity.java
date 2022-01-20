83d81387ac5a1c95d81be032690e61ac23665724
student <navgnbutk221@gmail.com>
2022-01-20 17:42:11 +0200

    end anim menu

--- app/src/main/java/com/example/homeworktests/MenuActivity.java
+++ app/src/main/java/com/example/homeworktests/MenuActivity.java
@@ -1,50 +1,80 @@
 package com.example.homeworktests;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
-    FloatingActionButton fab;
+
+    FloatingActionButton fab, fab1, fab2;
+    Animation fabOpen, fabClose, rotateForward, rotateBackward;
+    boolean isOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        init();
+
+    }
+
+
+    public void init() {
         fab = findViewById(R.id.fab);
+        fab1 = findViewById(R.id.fab1);
+        fab2 = findViewById(R.id.fab2);
+
+        fabOpen = AnimationUtils.loadAnimation
+                (this, R.anim.fab_open);
+        fabClose = AnimationUtils.loadAnimation
+                (this, R.anim.fab_close);
+        rotateForward = AnimationUtils.loadAnimation
+                (this, R.anim.rotate_forward);
+        rotateBackward = AnimationUtils.loadAnimation
+                (this, R.anim.rotate_backward);
+
         fab.setOnClickListener(this);
+        fab1.setOnClickListener(this);
+        fab2.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        if (v == fab)
-            registerForContextMenu(fab);
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.popup_menu, menu);
-    }
-
-    @Override
-    public boolean onContextItemSelected(@NonNull MenuItem item) {
-
-        if (item.getItemId() == R.id.add_homework) {
-            return true;
-        } else if (item.getItemId() == R.id.add_homework) {
-            return true;
+        if (v == fab) {
+            animateFab();
+        } else if (v == fab1) {
+            animateFab();
+            Toast.makeText(this, "click 1", Toast.LENGTH_SHORT).show();
+        } else if (v == fab2) {
+            animateFab();
+            Toast.makeText(this, "click 2", Toast.LENGTH_SHORT).show();
         }
-        return super.onContextItemSelected(item);
+
     }
 
-
+    private void animateFab() {
+        if (isOpen) {
+            fab.startAnimation(rotateForward);
+            fab1.startAnimation(fabClose);
+            fab2.startAnimation(fabClose);
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+            isOpen = false;
+        } else {
+            fab.startAnimation(rotateBackward);
+            fab1.startAnimation(fabOpen);
+            fab2.startAnimation(fabOpen);
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+            isOpen = true;
+        }
+    }
 }
