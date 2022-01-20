246c3e314f473b79f4cb28e5f959bfe79c53642b
User <navgnbutl221@gmail.com>
2022-01-20 15:02:59 +0200

    .

--- app/src/main/java/com/example/homeworktests/MenuActivity.java
+++ app/src/main/java/com/example/homeworktests/MenuActivity.java
@@ -11,7 +11,7 @@ import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MenuActivity extends AppCompatActivity{
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
     FloatingActionButton fab;
 
     @Override
@@ -19,30 +19,32 @@ public class MenuActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         fab = findViewById(R.id.fab);
-       registerForContextMenu(fab);
+        fab.setOnClickListener(this);
     }
 
-
-
+    @Override
+    public void onClick(View v) {
+        if (v == fab)
+            registerForContextMenu(fab);
+    }
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        MenuInflater menuInflater=getMenuInflater();
+        MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.popup_menu, menu);
     }
 
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId()==R.id.add_homework)
-        {
-            return true;
-        }
-       else if(item.getItemId()==R.id.add_homework)
-        {
-            return true;
-        }
 
-            return false;
+        if (item.getItemId() == R.id.add_homework) {
+            return true;
+        } else if (item.getItemId() == R.id.add_homework) {
+            return true;
+        }
+        return super.onContextItemSelected(item);
     }
+
+
 }
