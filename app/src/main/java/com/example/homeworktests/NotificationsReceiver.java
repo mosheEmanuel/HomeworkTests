33eb225a4cad592f74332a18c8a9aa7aeb754786
student <navgnbutk221@gmail.com>
2022-06-09 12:50:34 +0300

    .

--- app/src/main/java/com/example/homeworktests/NotificationsReceiver.java
+++ app/src/main/java/com/example/homeworktests/NotificationsReceiver.java
@@ -1,8 +1,13 @@
 package com.example.homeworktests;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -11,10 +16,27 @@ public class NotificationsReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"notify").
-                setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
-        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
-        notificationManagerCompat.notify(200, builder.build());
+
+        //phase 2
+        Intent intent1 = new Intent(context, MenuActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent1, 0);
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);//חיבור לservice של המערכת
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "M_CH_ID");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {//אם הגרסה מעל אוראו חובה להוסיף channel
+            String channelId = "YOUR_CHANNEL_ID";
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+            builder.setChannelId(channelId);
+        }
+        //phase 3
+//        Notification notification = builder.setContentIntent(pendingIntent)
+//                .setSmallIcon(icon).setTicker(ticker).setWhen(when)
+//                .setAutoCancel(true).setContentTitle(title)
+//                .setContentText(message).build();
+//        notificationManager.notify(3, notification);//יצירת האובייקט של ההתראה
     }
 }
